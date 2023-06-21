8c7cb857229a2af6b7bc7abc5d30bbbfb02011db
lil <1234@naver.com>
2023-06-21 16:23:33 +0900

    delete git

---
+++ src/main/reactjs/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import spongebob from "./spongebob.jpg";
+
+function App() {
+  return (
+    <div className="App">
+      <img alt="" src={spongebob} width={300} border="1" />
+      <br />
+      <br />
+      <h2>Docker 배포 성공 기원!!</h2>
+    </div>
+  );
+}
+
+export default App;
