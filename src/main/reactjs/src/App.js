9a3cc01870067a5b51388ed4f226ae917b9678ff
lil <1234@naver.com>
2023-06-22 16:15:25 +0900

    배포 후 커밋

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -4,7 +4,7 @@ import spongebob from "./spongebob.jpg";
 function App() {
   return (
     <div className="App">
-      <img alt="" src={spongebob} width={300} border="1" />
+      <img alt="" src={spongebob} width={1000} border="1" />
       <br />
       <br />
       <h2>Docker 배포 성공 기원!!</h2>
